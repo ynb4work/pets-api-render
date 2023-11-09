b81272fb6edd2861d5718bf3597489d1cc71ae77
Ynb925 <ynb4work@gmail.com>
2023-11-09 15:41:49 +0200

    add files

--- src/main/java/com/pets/homelesspetsapi/dto/mapper/PetCardMapper.java
+++ src/main/java/com/pets/homelesspetsapi/dto/mapper/PetCardMapper.java
@@ -9,18 +9,10 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PetCardMapper {
-<<<<<<< HEAD
-
-=======
->>>>>>> c9facf68ab80df0023172476a8d4db98e3060df8
     @Mapping(target = "id", ignore = true)
     PetCard mapToPetCard(PetCardDTO petCardDTO);
 
     PetCardDTO mapToPetCardDTO(PetCard petCard);
 
     List<PetCardDTO> mapToListPetCardDTO(List<PetCard> petCardList);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> c9facf68ab80df0023172476a8d4db98e3060df8
--- src/main/java/com/pets/homelesspetsapi/repository/PetCardRepository.java
+++ src/main/java/com/pets/homelesspetsapi/repository/PetCardRepository.java
@@ -3,12 +3,7 @@ package com.pets.homelesspetsapi.repository;
 import com.pets.homelesspetsapi.entity.PetCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< HEAD
 import java.util.UUID;
 
 public interface PetCardRepository extends JpaRepository<PetCard, UUID> {
-=======
-public interface PetCardRepository extends JpaRepository<PetCard, Long> {
-
->>>>>>> c9facf68ab80df0023172476a8d4db98e3060df8
 }
